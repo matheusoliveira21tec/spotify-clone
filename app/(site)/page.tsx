c33ea4e46998c3c7bd9670d5e094d6ff194c24df
monegasco <mhoa.2000@gmail.com>
2023-09-06 20:20:36 -0300

    pageContent

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,7 @@
 import getSongs from "@/actions/getSongs";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
+import PageContent from "./components/PageContent";
 export const revalidate = 0;
 export default async function Home() {
   const songs = await getSongs();
@@ -27,9 +28,7 @@ export default async function Home() {
             Newest songs
           </h1>
         </div>
-        {songs.map((song) => (
-          <div>{song.title}</div>
-        ))}
+        <PageContent songs={songs} />
       </div>
     </div>
   )

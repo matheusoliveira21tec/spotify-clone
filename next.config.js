0ece9f5ff6db2cbb22e57ed93dced554bcabb1c8
monegasco <mhoa.2000@gmail.com>
2023-09-06 20:29:18 -0300

    config next

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['tavgadbayhebgiwjupiy.supabase.co']
+    }
+}
 
 module.exports = nextConfig

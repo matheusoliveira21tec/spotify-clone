0ed6648b85f8ac71815c8278e03f31e2d38a2207
monegasco <mhoa.2000@gmail.com>
2023-09-06 21:11:41 -0300

    create hook usePlayer

--- components/Library.tsx
+++ components/Library.tsx
@@ -4,7 +4,14 @@ import { AiOutlinePlus } from "react-icons/ai";
 import { useUser } from "@/hooks/useUser";
 import useAuthModal from "@/hooks/useAuthModal";
 import useUploadModal from "@/hooks/useUploadModal";
-const Library = () => {
+import { Song } from "@/types";
+
+interface LibraryProps {
+    songs: Song[];
+}
+const Library: React.FC<LibraryProps> = ({
+    songs
+}) => {
     const { user, subscription } = useUser();
     const authModal = useAuthModal();
     const uploadModal = useUploadModal();
@@ -30,7 +37,11 @@ const Library = () => {
                     className=" text-neutral-400 cursor-pointer  hover:text-white transition"
                 />
             </div>
-
+            <div className="flex flex-col gap-y-2 mt-4 px-3">
+                {songs.map((item) => (
+                    <div>{item.title}</div>
+                ))}
+            </div>
         </div>
     );
 }

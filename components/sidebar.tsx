973465af6275c0ca62337578af486dd6260dbd78
monegasco <mhoa.2000@gmail.com>
2023-09-07 21:53:10 -0300

    integracao com stripe

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,11 +2,13 @@
 import { useMemo } from 'react';
 import { usePathname } from "next/navigation";
 import { BiSearch } from "react-icons/bi";
+import { twMerge } from "tailwind-merge";
 import { HiHome } from "react-icons/hi";
 import Box from './Box';
 import SidebarItem from "./SidebarItem";
 import Library from "./Library";
 import { Song } from '@/types';
+import usePlayer from '@/hooks/usePlayer';
 
 interface SidebarProps {
     children: React.ReactNode;
@@ -15,6 +17,7 @@ interface SidebarProps {
 
 const Sidebar = ({ children, songs }: SidebarProps) => {
     const pathname = usePathname();
+    const player = usePlayer();
     const routes = useMemo(() => [
         {
             icon: HiHome,
@@ -30,7 +33,7 @@ const Sidebar = ({ children, songs }: SidebarProps) => {
         },
     ], [pathname]);
     return (
-        <div className='flex h-full'>
+        <div className={twMerge(`flex h-full`, player.activeId && 'h-[calc(100%-80px)]')}>
             <div className='hidden md:flex flex-col gap-y-2  bg-black h-full w-[300px] p-2'>
                 <Box>   <div className="flex flex-col gap-y-4 px-5 py-4">
                     {routes.map((item) => (
